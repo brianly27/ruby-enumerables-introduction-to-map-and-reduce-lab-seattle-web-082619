0b5eee49d476eb33058935b48fb1031c2ed33082
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-31 18:27:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ end
 
 def reduce_to_all_true(array)
   counter = 1
-  value = array[0]
+  value = array[0].true?
   while counter < array.length
     if array[counter] && value
       value = true
