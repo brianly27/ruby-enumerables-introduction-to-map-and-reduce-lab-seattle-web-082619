501e2439d9218f553b5bbd022c99d911944df8a0
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-31 17:59:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,20 +21,11 @@ def map_to_no_change(array)
 end
 
 
-####  describe "map_to_double returns an array with the original values multiplied by 2" do
-####    it "transforms correctly" do
-  ####    expect(map_to_double([1, 2, 3, -9])).to eq([2, 4, 6, -18])
-
 def map_to_double(array)
   arr = []
   array.each { |e| arr << e*2}
   arr
 end
-
-
-####  describe "map_to_square returns an array with the original values squared" do
-  ####  it "transforms correctly" do
-  ####    expect(map_to_square([1, 2, 3, -9])).to eq([1, 4, 9, 81])
 
 def map_to_square(array)
   array = []
@@ -42,24 +33,10 @@ def map_to_square(array)
     array << e**2
   end
 end
-####  describe "reduce_to_total returns a running total when not given a starting point" do
-  ###  it "reduces correctly" do
-    ###  source_array = [1,2,3]
-    ###  expect(reduce_to_total(source_array)).to eq(6)
-
-###  describe "reduce_to_total returns a running total when given a starting point" do
-  ###  it "reduces correctly" do
-  ###    source_array = [1,2,3]
-    ###  starting_point = 100
-    ###  expect(reduce_to_total(source_array, starting_point)).to eq(106)
 
 def reduce_to_total(array, start = 0)
 
 end
-###  describe "reduce_to_all_true returns true when all values are truthy" do
-###    it "reduces correctly" do
-###      source_array = [1, 2, true, "razmatazz"]
-  ###    expect(reduce_to_all_true(source_array)).to be_truthy
 
 def reduce_to_all_true(array)
   counter = 0
@@ -69,7 +46,7 @@ def reduce_to_all_true(array)
       if e {value = true}
     end #each
   end #while
-end
+
 
 def reduce_to_any_true(array)
 
